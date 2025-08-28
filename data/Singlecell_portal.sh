7e032b81f16fb4a31a8be2882cd1d9c76c51d595
Semawule Syrus <137260746+Syrest123@users.noreply.github.com>
2025-08-28 09:23:16 +0300

    Create Singlecell_portal.sh

---
+++ data/Singlecell_portal.sh
@@ -0,0 +1,5 @@
+# Found 4 datasets
+Study1 - https://singlecell.broadinstitute.org/single_cell/study/SCP2699/multiome-demo-study-10x-cell-ranger-arc-processed
+Study2 - https://singlecell.broadinstitute.org/single_cell/study/SCP1859/multi-omic-profiling-of-the-developing-human-cerebral-cortex-at-the-single-cell-level
+Study3 - https://singlecell.broadinstitute.org/single_cell/study/SCP2012/a-systems-biology-approach-uncovers-novel-disease-mechanisms-in-age-related-macular-degeneration
+Study4 - https://singlecell.broadinstitute.org/single_cell/study/SCP2121/single-cell-regulomes-of-human-peripheral-blood-innate-lymphoid-cells-ilcs
