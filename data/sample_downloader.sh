e42b868bf1970d9e756c093336029fba94c91e00
Semawule Syrus <137260746+Syrest123@users.noreply.github.com>
2025-08-28 09:13:01 +0300

    Create sample_downloader.sh

---
+++ data/sample_downloader.sh
@@ -0,0 +1,21 @@
+# I have found 84 datasets on the 10X genomics database
+wget https://s3-us-west-2.amazonaws.com/10x.files/samples/cell-atac/2.1.0/10k_pbmc_ATACv2_nextgem_Chromium_Controller/10k_pbmc_ATACv2_nextgem_Chromium_Controller_fastqs.tar         # 48.1GB
+wget https://s3-us-west-2.amazonaws.com/10x.files/samples/cell-atac/2.1.0/10k_pbmc_ATACv2_nextgem_Chromium_X/10k_pbmc_ATACv2_nextgem_Chromium_X_fastqs.tar                           # 39.8GB
+wget https://s3-us-west-2.amazonaws.com/10x.files/samples/cell-atac/2.1.0/10k_pbmc_ATACv1p1_nextgem_Chromium_X/10k_pbmc_ATACv1p1_nextgem_Chromium_X_fastqs.tar                       # 44.9GB
+wget https://s3-us-west-2.amazonaws.com/10x.files/samples/cell-atac/2.0.0/10k_PBMC_ATAC_nextgem_Chromium_X/10k_PBMC_ATAC_nextgem_Chromium_X_fastqs.tar                               # 42.9GB
+wget https://s3-us-west-2.amazonaws.com/10x.files/samples/cell-atac/2.0.0/10k_PBMC_ATAC_nextgem_Chromium_Controller/10k_PBMC_ATAC_nextgem_Chromium_Controller_fastqs.tar             # 48.1GB
+# Both gene expression and ATAC data
+wget https://s3-us-west-2.amazonaws.com/10x.files/samples/cell-arc/2.0.0/10k_PBMC_Multiome_nextgem_Chromium_X/10k_PBMC_Multiome_nextgem_Chromium_X_fastqs.tar                        # 108GB
+wget https://cf.10xgenomics.com/samples/cell-arc/2.0.0/10k_PBMC_Multiome_nextgem_Chromium_X/10k_PBMC_Multiome_nextgem_Chromium_X_library.csv                                         # 353B
+
+#
+wget https://s3-us-west-2.amazonaws.com/10x.files/samples/cell-arc/2.0.0/pbmc_unsorted_3k/pbmc_unsorted_3k_fastqs.tar                                                                # 23.3GB
+wget https://cf.10xgenomics.com/samples/cell-arc/2.0.0/pbmc_unsorted_3k/pbmc_unsorted_3k_library.csv                                                                                 # 176B
+
+# 
+wget https://s3-us-west-2.amazonaws.com/10x.files/samples/cell-arc/1.0.0/pbmc_unsorted_10k/pbmc_unsorted_10k_fastqs.tar                                                             # 144GB
+wget https://cf.10xgenomics.com/samples/cell-arc/1.0.0/pbmc_unsorted_10k/pbmc_unsorted_10k_library.csv                                                                              # 180B
+
+#
+wget https://s3-us-west-2.amazonaws.com/10x.files/samples/cell-arc/1.0.0/pbmc_granulocyte_sorted_10k/pbmc_granulocyte_sorted_10k_fastqs.tar                                         # 95.6GB
+wget https://cf.10xgenomics.com/samples/cell-arc/1.0.0/pbmc_granulocyte_sorted_10k/pbmc_granulocyte_sorted_10k_library.csv                                                          # 220B
